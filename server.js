80e2969432f9d802c69fb5a517bb0fe054fd461c
Zach Frame <framez@bgsu.edu>
2018-05-22 13:51:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,22 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const db = require("./models");
 // Handles data parsing.
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(bodyParser.json())
 
+app.use(express.static("public"));
 
 // Router.
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Listener.
-app.listen(PORT, () => console.log('Server started on port ' + PORT));
+db.sequelize.synch({
+    force: true
+}).then(function () {
+    app.listen(PORT, () => console.log('Server started on port ' + PORT));
+})
